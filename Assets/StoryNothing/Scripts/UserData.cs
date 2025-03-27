4268a55c3f858332c31bfe78e4659ef96b203f74
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-27 23:11:52 +0900

    Enhance UserData class with inventory management methods

--- Assets/StoryNothing/Scripts/UserData.cs
+++ Assets/StoryNothing/Scripts/UserData.cs
@@ -1,10 +1,43 @@
-using System;
+using System.Collections.Generic;
 
 namespace StoryNothing
 {
-    [Serializable]
     public class UserData
     {
+        private readonly Dictionary<int, int> inventory = new();
 
+        public void AddItem(int itemId, int count)
+        {
+            if (inventory.ContainsKey(itemId))
+            {
+                inventory[itemId] += count;
+            }
+            else
+            {
+                inventory[itemId] = count;
+            }
+        }
+
+        public void RemoveItem(int itemId, int count)
+        {
+            if (inventory.ContainsKey(itemId))
+            {
+                inventory[itemId] -= count;
+                if (inventory[itemId] <= 0)
+                {
+                    inventory.Remove(itemId);
+                }
+            }
+        }
+
+        public int GetItemCount(int itemId)
+        {
+            return inventory.ContainsKey(itemId) ? inventory[itemId] : 0;
+        }
+
+        public bool SatisfactionCheck(int itemId, int count)
+        {
+            return inventory.ContainsKey(itemId) && inventory[itemId] >= count;
+        }
     }
 }
