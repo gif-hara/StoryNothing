0f65ffe6c5fd02fe52e27dafc6cc4324d8da2f0b
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-27 16:04:24 +0900

    Update Actor prefab and script to adjust character controller settings and initialize movement controller

--- Assets/StoryNothing/Scripts/ActorControllers/Actor.cs
+++ Assets/StoryNothing/Scripts/ActorControllers/Actor.cs
@@ -1,8 +1,18 @@
+using StandardAssets.Characters.Physics;
 using UnityEngine;
 
 namespace StoryNothing.ActorControllers
 {
     public class Actor : MonoBehaviour
     {
+        [SerializeField]
+        private OpenCharacterController characterController;
+
+        public ActorMovementController MovementController { get; private set; }
+
+        void Start()
+        {
+            MovementController = new ActorMovementController(this, characterController);
+        }
     }
 }
