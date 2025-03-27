469005ce0004d80a0bb3a4cd6b68ab7f0eeeadf9
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-27 15:05:51 +0900

    Add Actor prefab and update GameSceneController to instantiate player

--- Assets/StoryNothing/Scripts/GameSceneController.cs
+++ Assets/StoryNothing/Scripts/GameSceneController.cs
@@ -1,8 +1,22 @@
+using StoryNothing.ActorControllers;
 using UnityEngine;
 
 namespace StoryNothing
 {
     public class GameSceneController : MonoBehaviour
     {
+        [SerializeField]
+        private Actor playerPrefab;
+
+        [SerializeField]
+        private Transform playerSpawnPoint;
+
+        [SerializeField]
+        private Transform playerParent;
+
+        private void Start()
+        {
+            Instantiate(playerPrefab, playerSpawnPoint.position, playerSpawnPoint.rotation, playerParent);
+        }
     }
 }
