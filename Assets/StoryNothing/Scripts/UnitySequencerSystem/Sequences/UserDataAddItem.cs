151ee81d43b52ec008b7035748cbeecaf78bbdfc
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-28 10:12:29 +0900

    Add UserDataAddItem class for sequence handling; include associated meta files

---
+++ Assets/StoryNothing/Scripts/UnitySequencerSystem/Sequences/UserDataAddItem.cs
@@ -0,0 +1,15 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+using UnitySequencerSystem;
+
+namespace StoryNothing
+{
+    public class UserDataAddItem : ISequence
+    {
+        public UniTask PlayAsync(Container container, CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
