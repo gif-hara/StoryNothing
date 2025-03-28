e6761cebeb1af5de18e3317a4598eaafaa84e975
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-28 10:15:44 +0900

    Add Serializable attribute to UserDataAddItem class for improved serialization

--- Assets/StoryNothing/Scripts/UnitySequencerSystem/Sequences/UserDataAddItem.cs
+++ Assets/StoryNothing/Scripts/UnitySequencerSystem/Sequences/UserDataAddItem.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
@@ -6,6 +7,7 @@ using UnitySequencerSystem;
 
 namespace StoryNothing
 {
+    [Serializable]
     public class UserDataAddItem : ISequence
     {
         [SerializeField]
