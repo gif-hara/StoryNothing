a5d7aa7b6e86773fd6dd3f6008d8caf5c33d9d0f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-03-31 11:44:57 +0900

    Add properties and constructor to BattleActor; initialize players and enemies in BattleController

--- Assets/StoryNothing/Scripts/BattleSystems/BattleActor.cs
+++ Assets/StoryNothing/Scripts/BattleSystems/BattleActor.cs
@@ -4,5 +4,14 @@ namespace StoryNothing.BattleSystems
 {
     public class BattleActor
     {
+        public string Name { get; set; }
+
+        public int HitPoint { get; set; }
+
+        public BattleActor(string name, int hitPoint)
+        {
+            Name = name;
+            HitPoint = hitPoint;
+        }
     }
 }
--- Assets/StoryNothing/Scripts/BattleSystems/BattleController.cs
+++ Assets/StoryNothing/Scripts/BattleSystems/BattleController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Cysharp.Threading.Tasks;
 using HK;
 using MH3;
@@ -8,9 +9,17 @@ namespace StoryNothing.BattleSystems
 {
     public class BattleController
     {
+        public Party Players { get; } = new Party();
+
+        public Party Enemies { get; } = new Party();
+
         public async UniTask BeginAsync(HKUIDocument battleDocumentPrefab)
         {
             var battleDocument = UnityEngine.Object.Instantiate(battleDocumentPrefab);
+            Players.Add(new BattleActor("Player1", 100));
+            Players.Add(new BattleActor("Player2", 100));
+            Enemies.Add(new BattleActor("Enemy1", 100));
+            Enemies.Add(new BattleActor("Enemy2", 100));
             await UniTask.Delay(TimeSpan.FromSeconds(2f));
             battleDocument.DestroySafe();
         }
