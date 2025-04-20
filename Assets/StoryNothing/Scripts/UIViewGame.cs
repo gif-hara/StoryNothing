2a90b20241188232feea38e6709a26933518fbb9
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-20 17:32:12 +0900

    Refactor UIViewGame to use documentPrefab and improve button management functionality

--- Assets/StoryNothing/Scripts/UIViewGame.cs
+++ Assets/StoryNothing/Scripts/UIViewGame.cs
@@ -1,52 +1,93 @@
+using System.Collections.Generic;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using HK;
 using MH3;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace StoryNothing
 {
     public class UIViewGame
     {
-        public readonly HKUIDocument backgroundDocumentPrefab;
+        public readonly HKUIDocument documentPrefab;
 
-        private HKUIDocument backgroundDocument;
+        private HKUIDocument document;
+
+        private HKUIDocument areaButtonsDocument;
+
+        private HKUIDocument buttonPrefab;
 
         public UIViewGame(HKUIDocument backgroundDocumentPrefab)
         {
-            this.backgroundDocumentPrefab = backgroundDocumentPrefab;
+            this.documentPrefab = backgroundDocumentPrefab;
         }
 
         public void Setup(CancellationToken cancellationToken)
         {
-            backgroundDocument = Object.Instantiate(backgroundDocumentPrefab);
-            backgroundDocument.gameObject.SetActive(true);
+            document = Object.Instantiate(documentPrefab);
+            document.gameObject.SetActive(true);
+            areaButtonsDocument = document.Q<HKUIDocument>("Area.Buttons");
+            buttonPrefab = areaButtonsDocument.Q<HKUIDocument>("Prefab.Button");
             cancellationToken.RegisterWithoutCaptureExecutionContext(() =>
             {
-                backgroundDocument.DestroySafe();
+                document.DestroySafe();
             });
         }
 
         public void Open()
         {
-            if (backgroundDocument == null)
+            if (document == null)
             {
                 Debug.LogError("Background document is not set up.");
                 return;
             }
 
-            backgroundDocument.gameObject.SetActive(true);
+            document.gameObject.SetActive(true);
         }
 
         public void Close()
         {
-            if (backgroundDocument == null)
+            if (document == null)
             {
                 Debug.LogError("Background document is not set up.");
                 return;
             }
 
-            backgroundDocument.gameObject.SetActive(false);
+            document.gameObject.SetActive(false);
+        }
+
+        public async UniTask<int> CreateButtons(IEnumerable<string> buttonTexts, CancellationToken cancellationToken)
+        {
+            if (buttonTexts == null)
+            {
+                Debug.LogError("Button texts cannot be null.");
+                return -1;
+            }
+
+            DestroyButtonAll();
+
+            return await UniTask.WhenAny(buttonTexts
+                .Select(x =>
+                {
+                    var button = Object.Instantiate(buttonPrefab, areaButtonsDocument.transform);
+                    button.Q<TMP_Text>("Text").text = x;
+                    return button.Q<Button>("Button").OnClickAsync();
+                })
+            );
+        }
+
+        public void DestroyButtonAll()
+        {
+            for (var i = 0; i < areaButtonsDocument.transform.childCount; i++)
+            {
+                var child = areaButtonsDocument.transform.GetChild(i);
+                if (child != null)
+                {
+                    Object.Destroy(child.gameObject);
+                }
+            }
         }
     }
 }
