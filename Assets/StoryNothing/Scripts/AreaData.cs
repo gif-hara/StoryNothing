42fbacc771d6fdae0b8355ca921b1d5284bdd52e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-20 12:09:44 +0900

    Add AreaData ScriptableObject for area management

---
+++ Assets/StoryNothing/Scripts/AreaData.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace StoryNothing
+{
+    [CreateAssetMenu(fileName = "AreaData", menuName = "StoryNothing/AreaData")]
+    public class AreaData : ScriptableObject
+    {
+    }
+}
