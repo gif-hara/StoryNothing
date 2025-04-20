85ad0187d8be46e91f3a2508d1e17fb11c79d2e0
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-20 10:27:38 +0900

    Refactor GameSceneController by removing unused fields and methods to streamline initialization process

--- Assets/StoryNothing/Scripts/GameSceneController.cs
+++ Assets/StoryNothing/Scripts/GameSceneController.cs
@@ -1,12 +1,5 @@
-using System.ComponentModel.Design;
-using Cysharp.Threading.Tasks;
 using HK;
-using R3;
-using R3.Triggers;
-using StoryNothing.ActorControllers;
-using StoryNothing.BattleSystems;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 namespace StoryNothing
 {
@@ -15,57 +8,11 @@ namespace StoryNothing
         [SerializeField]
         private GameRules gameRules;
 
-        [SerializeField]
-        private Actor playerPrefab;
-
-        [SerializeField]
-        private Transform playerSpawnPoint;
-
-        [SerializeField]
-        private Transform playerParent;
-
-        [SerializeField]
-        private FieldCameraController fieldCameraControllerPrefab;
-
-        [SerializeField]
-        private BattleDebugData battleDebugData;
-
-        [SerializeField]
-        private HKUIDocument battleDocumentPrefab;
-
-        private BattleController battleController;
-
         private void Start()
         {
             ServiceLocator.Register(gameRules, destroyCancellationToken);
             ServiceLocator.Register(new InputController(), destroyCancellationToken);
             ServiceLocator.Register(new UserData(), destroyCancellationToken);
-            var player = Instantiate(playerPrefab, playerSpawnPoint.position, playerSpawnPoint.rotation, playerParent);
-            var fieldCameraController = Instantiate(fieldCameraControllerPrefab);
-            fieldCameraController.SetDefaultTrackingTarget(player.transform);
-            ServiceLocator.Register(fieldCameraController, destroyCancellationToken);
-            PlayerController.Attach(player, destroyCancellationToken);
-
-            this.UpdateAsObservable()
-                .Subscribe(this, static (_, @this) =>
-                {
-                    if (Keyboard.current.digit1Key.wasPressedThisFrame)
-                    {
-                        @this.BeginBattleAsync().Forget();
-                    }
-                })
-                .RegisterTo(destroyCancellationToken);
-        }
-
-        private async UniTask BeginBattleAsync()
-        {
-            if (battleController != null)
-            {
-                return;
-            }
-            battleController = new BattleController(battleDebugData);
-            await battleController.BeginAsync(battleDocumentPrefab);
-            battleController = null;
         }
     }
 }
