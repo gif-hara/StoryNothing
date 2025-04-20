7855c46e9a9b66bef78d371161b44fddae80d9d2
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-04-21 00:50:27 +0900

    Add CollectionSpot class to manage collection points and item weights

---
+++ Assets/StoryNothing/Scripts/CollectionSpot.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using HK;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace StoryNothing
+{
+    /// <summary>
+    /// 採集ポイント
+    /// </summary>
+    public class CollectionSpot
+    {
+        /// <summary>
+        /// 採集可能回数
+        /// </summary>
+        private int count;
+
+        private readonly List<ItemWeight> itemWeights;
+
+        public CollectionSpot(int count, List<ItemWeight> itemWeights)
+        {
+            this.count = count;
+            this.itemWeights = itemWeights;
+        }
+
+        public int Collection()
+        {
+            Assert.IsTrue(count >= 0, "採集可能回数が負の値になっています。");
+            count--;
+
+            return itemWeights.Lottery(i => i.Weight).ItemId;
+        }
+
+        public bool CanCollection()
+        {
+            return count > 0;
+        }
+    }
+}
