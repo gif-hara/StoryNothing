0cb12896502c2c5cec95e7b8e097b425b0b3fe04
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-23 16:28:37 +0900

    UserDataクラスをシリアライズ可能にし、スキルボードの追加・削除機能を実装

--- Assets/StoryNothing/Scripts/Runtime/UserData.cs
+++ Assets/StoryNothing/Scripts/Runtime/UserData.cs
@@ -1,8 +1,28 @@
+using System;
 using System.Collections.Generic;
+using StoryNothing.InstanceData;
+using UnityEngine;
 
 namespace StoryNothing
 {
-    public class UserData
+    [Serializable]
+    public sealed class UserData
     {
+        [field: SerializeField]
+        public List<InstanceSkillBoard> SkillBoards { get; private set; } = new();
+
+        [field: SerializeField]
+        public int AddInstanceSkillBoardCount { get; private set; } = 0;
+
+        public void AddSkillBoard(InstanceSkillBoard skillBoard)
+        {
+            SkillBoards.Add(skillBoard);
+            AddInstanceSkillBoardCount++;
+        }
+
+        public void RemoveSkillBoard(InstanceSkillBoard skillBoard)
+        {
+            SkillBoards.Remove(skillBoard);
+        }
     }
 }
