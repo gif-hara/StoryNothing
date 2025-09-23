782ed9fb1e2ad84744518edca38984da4d10de65
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-23 16:21:21 +0900

    InstanceSkillBoardクラスとメタファイルを新規作成

---
+++ Assets/StoryNothing/Scripts/Runtime/InstanceData/InstanceSkillBoard.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+namespace StoryNothing
+{
+    public class InstanceSkillBoard : MonoBehaviour
+    {
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
