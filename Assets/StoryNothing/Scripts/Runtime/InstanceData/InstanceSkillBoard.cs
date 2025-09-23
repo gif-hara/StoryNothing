3edc85b017e6f52f3da9309e0365b116d15c6bb9
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-23 16:37:19 +0900

    InstanceSkillBoardクラスのSkillBoardMasterDataIdプロパティ名を修正し、コンストラクタとCreateメソッドを整理

--- Assets/StoryNothing/Scripts/Runtime/InstanceData/InstanceSkillBoard.cs
+++ Assets/StoryNothing/Scripts/Runtime/InstanceData/InstanceSkillBoard.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using StoryNothing.MasterDataSystems;
 using UnityEngine;
 
 namespace StoryNothing.InstanceData
@@ -11,9 +12,27 @@ namespace StoryNothing.InstanceData
         public int InstanceId { get; private set; }
 
         [field: SerializeField]
-        public int MasterDataSkillBoardId { get; private set; }
+        public int SkillBoardMasterDataId { get; private set; }
 
         [field: SerializeField]
         public List<Vector2Int> Holes { get; private set; } = new();
+
+        public InstanceSkillBoard(int instanceId, int masterDataSkillBoardId, List<Vector2Int> holes = null)
+        {
+            InstanceId = instanceId;
+            SkillBoardMasterDataId = masterDataSkillBoardId;
+            Holes = holes ?? new List<Vector2Int>();
+        }
+
+        public static InstanceSkillBoard Create(MasterData masterData, int instanceId, int skillBoardMasterDataId)
+        {
+            var holes = new List<Vector2Int>();
+            var skillBoardSpec = masterData.SkillBoardSpecs.Get(skillBoardMasterDataId);
+            for (int i = 0; i < skillBoardSpec.HoleCount; i++)
+            {
+                holes.Add(new Vector2Int(UnityEngine.Random.Range(0, skillBoardSpec.X), UnityEngine.Random.Range(0, skillBoardSpec.Y)));
+            }
+            return new InstanceSkillBoard(instanceId, skillBoardMasterDataId, holes);
+        }
     }
 }
