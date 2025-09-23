6eec94cf3e677eac2da0a6c1bd6496437d5c3e67
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-23 16:24:28 +0900

    UserDataクラスからアイテム管理機能を削除

--- Assets/StoryNothing/Scripts/Runtime/UserData.cs
+++ Assets/StoryNothing/Scripts/Runtime/UserData.cs
@@ -4,40 +4,5 @@ namespace StoryNothing
 {
     public class UserData
     {
-        public readonly Dictionary<int, int> Items = new();
-
-        public void AddItem(int itemId, int count)
-        {
-            if (Items.ContainsKey(itemId))
-            {
-                Items[itemId] += count;
-            }
-            else
-            {
-                Items[itemId] = count;
-            }
-        }
-
-        public void RemoveItem(int itemId, int count)
-        {
-            if (Items.ContainsKey(itemId))
-            {
-                Items[itemId] -= count;
-                if (Items[itemId] <= 0)
-                {
-                    Items.Remove(itemId);
-                }
-            }
-        }
-
-        public int GetItemCount(int itemId)
-        {
-            return Items.ContainsKey(itemId) ? Items[itemId] : 0;
-        }
-
-        public bool SatisfactionCheck(int itemId, int count)
-        {
-            return Items.ContainsKey(itemId) && Items[itemId] >= count;
-        }
     }
 }
