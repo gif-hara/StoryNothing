214d6d6c1d5eabccac6092ce8300db19d9d27238
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-28 08:26:25 +0900

    SkillPieceCellSpecクラスにSizeプロパティを追加

--- Assets/StoryNothing/Scripts/Runtime/MasterDataSystems/SkillPieceCellSpec.cs
+++ Assets/StoryNothing/Scripts/Runtime/MasterDataSystems/SkillPieceCellSpec.cs
@@ -15,6 +15,16 @@ namespace StoryNothing.MasterDataSystems
 
         public List<Vector2Int> CellPoints => ServiceLocator.Resolve<MasterData>().SkillPieceCellPoints.Get(Id).Select(x => new Vector2Int(x.X, x.Y)).ToList();
 
+        public Vector2Int Size
+        {
+            get
+            {
+                var xMax = CellPoints.Count > 0 ? CellPoints.Max(x => x.x) + 1 : 0;
+                var yMax = CellPoints.Count > 0 ? CellPoints.Max(x => x.y) + 1 : 0;
+                return new Vector2Int(xMax, yMax);
+            }
+        }
+
         [Serializable]
         public sealed class DictionaryList : DictionaryList<int, SkillPieceCellSpec>
         {
