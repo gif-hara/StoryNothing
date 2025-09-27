c3ae20018f808ea6cce421fcd0c5070eee4e9c3d
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-09-27 20:27:40 +0900

    SkillPieceSpecにSkillPieceCellSpecGroupIdプロパティを追加

--- Assets/StoryNothing/Scripts/Runtime/MasterDataSystems/SkillPieceSpec.cs
+++ Assets/StoryNothing/Scripts/Runtime/MasterDataSystems/SkillPieceSpec.cs
@@ -10,6 +10,8 @@ namespace StoryNothing.MasterDataSystems
 
         public string Name;
 
+        public int SkillPieceCellSpecGroupId;
+
         [Serializable]
         public sealed class DictionaryList : DictionaryList<int, SkillPieceSpec>
         {
