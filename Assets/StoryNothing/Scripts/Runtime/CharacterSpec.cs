9b78a817d905f80b06488880fff0b304cf2ad91e
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-02 23:46:45 +0900

    CharacterSpecクラスを新規作成し、キャラクターのステータスを管理するプロパティを追加

---
+++ Assets/StoryNothing/Scripts/Runtime/CharacterSpec.cs
@@ -0,0 +1,30 @@
+namespace StoryNothing
+{
+    public sealed class CharacterSpec
+    {
+        public int HitPoint { get; private set; }
+
+        public int MagicPoint { get; private set; }
+
+        public int PhysicalAttack { get; private set; }
+
+        public int PhysicalDefense { get; private set; }
+
+        public int MagicalAttack { get; private set; }
+
+        public int MagicalDefense { get; private set; }
+
+        public int Speed { get; private set; }
+
+        public CharacterSpec(int hitPoint, int magicPoint, int physicalAttack, int physicalDefense, int magicalAttack, int magicalDefense, int speed)
+        {
+            HitPoint = hitPoint;
+            MagicPoint = magicPoint;
+            PhysicalAttack = physicalAttack;
+            PhysicalDefense = physicalDefense;
+            MagicalAttack = magicalAttack;
+            MagicalDefense = magicalDefense;
+            Speed = speed;
+        }
+    }
+}
