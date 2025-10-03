4e150ac41ac3638a9523a6735dbfaf24ef01c55a
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-03 21:03:29 +0900

    InstanceCharacterクラスのCharacterSpecIdプロパティを修正し、関連するプロパティとメソッドを追加

--- Assets/StoryNothing/Scripts/Runtime/InstanceData/InstanceCharacter.cs
+++ Assets/StoryNothing/Scripts/Runtime/InstanceData/InstanceCharacter.cs
@@ -1,9 +1,33 @@
+using HK;
+using StoryNothing.MasterDataSystems;
 using UnityEngine;
 
 namespace StoryNothing.InstanceData
 {
     public sealed class InstanceCharacter
     {
-        public int CharacterSpecId;
+        public int CharacterSpecId { get; private set; }
+
+        public CharacterSpec CharacterSpec { get; private set; }
+
+        public int HitPointMax => CharacterSpec.HitPoint;
+
+        public int MagicPointMax => CharacterSpec.MagicPoint;
+
+        public int CurrentPhysicalAttack => CharacterSpec.PhysicalAttack;
+
+        public int CurrentPhysicalDefense => CharacterSpec.PhysicalDefense;
+
+        public int CurrentMagicalAttack => CharacterSpec.MagicalAttack;
+
+        public int CurrentMagicalDefense => CharacterSpec.MagicalDefense;
+
+        public int CurrentSpeed => CharacterSpec.Speed;
+
+        public InstanceCharacter(int characterSpecId)
+        {
+            CharacterSpecId = characterSpecId;
+            CharacterSpec = ServiceLocator.Resolve<MasterData>().CharacterSpecs.Get(CharacterSpecId);
+        }
     }
 }
