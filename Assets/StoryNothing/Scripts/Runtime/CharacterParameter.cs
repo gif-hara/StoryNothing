a00c3f753e7cd94ac859f52f7904e0b8a21c2722
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-03 21:09:57 +0900

    CharacterParameter構造体を新規作成し、基本ステータス計算を実装

---
+++ Assets/StoryNothing/Scripts/Runtime/CharacterParameter.cs
@@ -0,0 +1,20 @@
+namespace StoryNothing
+{
+    public struct CharacterParameter
+    {
+        public int Base;
+
+        public int Additional;
+
+        public float AdditionalRate;
+
+        public int Current => Base + Additional + (int)(Base * AdditionalRate);
+
+        public CharacterParameter(int baseValue)
+        {
+            Base = baseValue;
+            Additional = 0;
+            AdditionalRate = 0f;
+        }
+    }
+}
