a08908ec9c26f459bc7fafe094f0b49adec9b890
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-13 13:35:56 +0900

    UIElementBingoLine構造体にRectTransformを追加し、水平・垂直ラインの設定メソッドを実装

--- Assets/StoryNothing/Scripts/Runtime/UIElementBingoLine.cs
+++ Assets/StoryNothing/Scripts/Runtime/UIElementBingoLine.cs
@@ -1,5 +1,6 @@
 using System;
 using HK;
+using UnityEngine;
 
 namespace StoryNothing
 {
@@ -7,14 +8,30 @@ namespace StoryNothing
     {
         private readonly HKUIDocument document;
 
+        private readonly RectTransform rectTransform;
+
         public UIElementBingoLine(HKUIDocument document)
         {
             this.document = document;
+            this.rectTransform = document.GetComponent<RectTransform>();
         }
 
         public void Dispose()
         {
             UnityEngine.Object.Destroy(document.gameObject);
+        }
+
+        public void SetupAsHorizontalLine(int index, Vector2Int skillBoardSize)
+        {
+            var cellSize = Define.CellSize;
+            rectTransform.localPosition = new Vector3(0, (skillBoardSize.y - 1) * cellSize / 2f - index * cellSize, 0);
+        }
+
+        public void SetupAsVerticalLine(int index, Vector2Int skillBoardSize)
+        {
+            var cellSize = Define.CellSize;
+            rectTransform.localPosition = new Vector3(-(skillBoardSize.x - 1) * cellSize / 2f + index * cellSize, 0, 0);
+            rectTransform.localEulerAngles = new Vector3(0, 0, 90);
         }
     }
 }
