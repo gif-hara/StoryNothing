1230ddfbe64a408ba7dc2a1124ae8304e78f90ef
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-18 09:30:41 +0900

    UIElementNotificationクラスにdocumentフィールドを追加し、Addメソッドを修正

--- Assets/StoryNothing/Scripts/Runtime/UIElemenNotification.cs
+++ Assets/StoryNothing/Scripts/Runtime/UIElemenNotification.cs
@@ -1,4 +1,7 @@
+using System.Threading;
+using Cysharp.Threading.Tasks;
 using HK;
+using LitMotion;
 using TMPro;
 using UnityEngine;
 
@@ -6,21 +9,33 @@ namespace StoryNothing
 {
     public sealed class UIElementNotification
     {
+        private readonly HKUIDocument document;
+
         private readonly RectTransform labelParent;
 
         private readonly HKUIDocument labelPrefab;
 
         public UIElementNotification(HKUIDocument document)
         {
+            this.document = document;
             labelParent = document.Q<RectTransform>("Area.Label");
             labelPrefab = document.Q<HKUIDocument>("UI.Element.Notification");
         }
 
         public void Add(string message)
         {
+            AddInternalAsync(message, document.destroyCancellationToken).Forget();
+        }
+
+        private async UniTask AddInternalAsync(string message, CancellationToken cancellationToken)
+        {
             var labelInstance = Object.Instantiate(labelPrefab, labelParent);
             var textComponent = labelInstance.Q<TMP_Text>("Message");
             textComponent.text = message;
+
+            await UniTask.Delay(3000, cancellationToken: cancellationToken);
+
+            Object.Destroy(labelInstance.gameObject);
         }
     }
 }
