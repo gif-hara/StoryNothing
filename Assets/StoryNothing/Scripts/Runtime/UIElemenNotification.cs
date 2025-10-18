e5a94112198cda5ba276a258614f92aa8407e595
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-18 09:45:17 +0900

    UIElementNotificationにアニメーション効果を追加し、通知メッセージの表示を改善

--- Assets/StoryNothing/Scripts/Runtime/UIElemenNotification.cs
+++ Assets/StoryNothing/Scripts/Runtime/UIElemenNotification.cs
@@ -30,10 +30,31 @@ namespace StoryNothing
         private async UniTask AddInternalAsync(string message, CancellationToken cancellationToken)
         {
             var labelInstance = Object.Instantiate(labelPrefab, labelParent);
-            var textComponent = labelInstance.Q<TMP_Text>("Message");
-            textComponent.text = message;
+            var text = labelInstance.Q<TMP_Text>("Message");
+            text.text = message;
+            var content = labelInstance.Q<RectTransform>("Contents");
+            var canvasGroup = labelInstance.Q<CanvasGroup>("Contents");
 
+            await LMotion.Create(0.0f, 1.0f, 0.2f)
+                .WithEase(Ease.OutQuad)
+                .Bind(content, static (x, content) =>
+                {
+                    var anchorMin = content.anchorMin;
+                    anchorMin.x = Mathf.Lerp(1.0f, 0.0f, x);
+                    content.anchorMin = anchorMin;
+                    var anchorMax = content.anchorMax;
+                    anchorMax.x = Mathf.Lerp(2.0f, 1.0f, x);
+                    content.anchorMax = anchorMax;
+                })
+                .ToUniTask(cancellationToken: cancellationToken);
             await UniTask.Delay(3000, cancellationToken: cancellationToken);
+            await LMotion.Create(1.0f, 0.0f, 0.2f)
+                .WithEase(Ease.InQuad)
+                .Bind(canvasGroup, static (x, canvasGroup) =>
+                {
+                    canvasGroup.alpha = x;
+                })
+                .ToUniTask(cancellationToken: cancellationToken);
 
             Object.Destroy(labelInstance.gameObject);
         }
