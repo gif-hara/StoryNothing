e64721a575adf3bbad4982510669d13c0e8abe3f
gif-hara <hk.mp5k.a4.pdw83@gmail.com>
2025-10-28 00:24:12 +0900

    SkillPieceFilterDataのフィールドに初期値を設定

--- Assets/StoryNothing/Scripts/Runtime/SkillPieceFilterData.cs
+++ Assets/StoryNothing/Scripts/Runtime/SkillPieceFilterData.cs
@@ -2,10 +2,21 @@ namespace StoryNothing
 {
     public class SkillPieceFilterData
     {
-        public int cellNumber;
+        public int cellNumber = 0;
 
-        public Define.SkillPieceColor color;
+        public Define.SkillPieceColor color = Define.SkillPieceColor.Gray;
 
-        public string cellName;
+        public string cellName = string.Empty;
+
+        public SkillPieceFilterData()
+        {
+        }
+
+        public SkillPieceFilterData(SkillPieceFilterData other)
+        {
+            cellNumber = other.cellNumber;
+            color = other.color;
+            cellName = other.cellName;
+        }
     }
 }
